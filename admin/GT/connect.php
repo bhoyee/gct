026fb0d8419b2d2adc63e03b7670054823b9d121
unknown <s.adeboye@outlook.com>
2024-05-21 18:48:18 +0100

    updated trip information chart

--- admin/GT/connect.php
+++ admin/GT/connect.php
@@ -1,21 +1,46 @@
-<?php
-//$servername = "localhost";$servername = "localhost";
-$servername = "localhost";
-// Enter your MySQL username below(default=root)
-//$username = "xxxxxxxxx";
-//$username = 'xxxxxxx';
-// Enter your MySQL password below
-//$password = "xxxxxx";
-//$password = 'xxxxxx';
-//$dbname = "xxxxxxxxxx";
-//$dbname = 'xxxxxx';$servername = 'localhost';        $username = 'root';        $password = '';        $dbnamease = 'gt';
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    header("location:connection_error.php?error=$conn->connect_error");
-    die($conn->connect_error);
-}
-?>
+<?php
+
+//$servername = "localhost";$servername = "localhost";
+
+$servername = "localhost";
+
+// Enter your MySQL username below(default=root)
+
+//$username = "xxxxxxxxx";
+
+//$username = 'xxxxxxx';
+
+// Enter your MySQL password below
+
+//$password = "xxxxxx";
+
+//$password = 'xxxxxx';
+
+//$dbname = "xxxxxxxxxx";
+
+//$dbname = 'xxxxxx';
+$servername = 'localhost';
+        $username = 'root';
+        $password = '';
+        $dbname = 'gt';
+
+
+
+// Create connection
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+
+
+// Check connection
+
+if ($conn->connect_error) {
+
+    header("location:connection_error.php?error=$conn->connect_error");
+
+    die($conn->connect_error);
+
+}
+
+?>
+
--- admin/GT/getData.php
+++ admin/GT/getData.php
@@ -1,15 +1,29 @@
-<?php
-$servername='localhost';
-$username="giddycru_root";
-$password="giddyhost_01";
-try
-{
-    $con=new PDO("mysql:host=$servername;dbname=giddycru_gt",$username,$password);
-    $con->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-    //echo 'connected';
-}
-catch(PDOException $e)
-{
-    echo '<br>'.$e->getMessage();
-}
+<?php
+
+$servername='localhost';
+
+$username="root";
+
+$password="";
+
+try
+
+{
+
+    $con=new PDO("mysql:host=$servername;dbname=gt",$username,$password);
+
+    $con->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+    //echo 'connected';
+
+}
+
+catch(PDOException $e)
+
+{
+
+    echo '<br>'.$e->getMessage();
+
+}
+
 ?>
